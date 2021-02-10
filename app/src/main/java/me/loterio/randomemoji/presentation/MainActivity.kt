2956005f7cbceb477a4db4cb9a4a90db74418e87
Natan Loterio <natanloterio@gmail.com>
2021-02-10 17:42:50 -0300

    Initial commit

---
+++ app/src/main/java/me/loterio/randomemoji/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.loterio.randomemoji.presentation
+
+import android.app.Activity
+import android.os.Bundle
+import me.loterio.randomemoji.R
+
+class MainActivity :  Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
