ac8e2a91fe5a8d1fef6e68cc5bbdbd1e0491def5
Natan Lotério <natanloterio@gmail.com>
2021-02-17 21:05:13 -0300

    add: room database add: use local data when available instead of API calls add: build.gradle dependencies

--- app/src/main/java/me/loterio/randomemoji/di/RepositoryModule.kt
+++ app/src/main/java/me/loterio/randomemoji/di/RepositoryModule.kt
@@ -1,16 +1,35 @@
 package me.loterio.randomemoji.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import me.loterio.randomemoji.repository.impl.network.EmojiAPIService
 import me.loterio.randomemoji.repository.EmojisRepositoryImpl
+import me.loterio.randomemoji.repository.impl.db.AppRoomDatabase
+import me.loterio.randomemoji.repository.impl.db.dao.EmojiDao
 
 @Module
 class RepositoryModule {
 
     @Provides
-    fun provideEmojisRepository(emojiEmojiApi: EmojiAPIService): EmojisRepositoryImpl {
-       return  EmojisRepositoryImpl(emojiEmojiApi)
+    fun provideAppRoomDatabase(applicationContext: Context):AppRoomDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            AppRoomDatabase::class.java, "database-test"
+        ).build()
+    }
+
+    @Provides
+    fun provideEmojiDao(appRoomDatabase: AppRoomDatabase): EmojiDao {
+        return appRoomDatabase.emojiDao()
+    }
+
+    @Provides
+    fun provideEmojisRepository(
+        emojiEmojiApi: EmojiAPIService,
+        emojiDao: EmojiDao): EmojisRepositoryImpl {
+       return  EmojisRepositoryImpl(emojiEmojiApi,emojiDao)
     }
 
 }
